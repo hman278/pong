a6f653bd4f8fa074f7f0a17d26d931752a632e54
Sargis <hyudyman@gmail.com>
2025-03-29 15:33:42 +0400

    BROKEN: half-way refactored the old code

--- src/main.c
+++ src/main.c
@@ -4,86 +4,99 @@
  *
  ********************************************************************************************/
 
-#include "math.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "raylib.h"
 #include "raymath.h"
 
-// Define the number of frames for our racket sprite
-#define NUM_FRAMES 2
+#define PLAYER_OFFSET_X 20.0f
+#define PLAYER_A_START_POS ((Vector2){PLAYER_OFFSET_X, GetScreenHeight() / 2.0f})
+#define PLAYER_B_START_POS \
+    ((Vector2){GetScreenHeight() - PLAYER_OFFSET_X, GetScreenHeight() / 2.0f})
+#define BALL_START_POS ((Vector2){GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f})
 
+typedef enum { MENU, PAUSED, WAITING, ONGOING } GameState;
+
+/* An entity needs a texture for visual representation and a position */
 typedef struct {
     Texture2D texture;
     Vector2 position;
-} Sprite;
+} Entity;
 
-Sprite PlayerA, PlayerB, Ball;
-Texture2D racketTexture;
-Texture2D ballTexture;
-Rectangle racketRec, ballRec;
+/* TODO: Make sure the memory alignment is right, and no memory is wasted */
+typedef struct {
+    Entity base;
+    float speed;
+    Rectangle rect;
+    /* Define the number of frames for our racket sprite */
+    int frame_count;
+    int score;
+} PlayerRacket;
 
-bool bBallLaunched = false;
+typedef struct {
+    Entity base;
+    float speed;
+    Rectangle rect;
+} Ball;
 
-const float racketSpeed = 5.f;
-const float ballSpeed = 5.f;
+/* Globals */
+GameState g_game_state;
 
-int PlayerAScore = 0;
-int PlayerBScore = 0;
+Texture2D g_racket_texture;
+Texture2D g_ball_texture;
 
-Vector2 ballDir = {1, 0};
+PlayerRacket g_player_a, g_player_b;
+int g_player_a_score, g_player_b_score;
+Ball g_ball;
 
-char *debugText;
-char *playerAScoreStr;
-char *playerBScoreStr;
+char *g_debug_text;
 
-void Start() {
-    racketTexture = LoadTexture("resources/racket.png");
-    ballTexture = LoadTexture("resources/ball.png");
-
-    PlayerA.texture = racketTexture;
-    PlayerB.texture = racketTexture;
-
-    Ball.texture = ballTexture;
-
-    racketRec =
-        (Rectangle){0, 0, (float)racketTexture.width, (float)racketTexture.height / NUM_FRAMES};
-    ballRec = (Rectangle){0, 0, (float)ballTexture.width, (float)ballTexture.height};
-
-    // Set the initial player position
-    PlayerA.position = (Vector2){20.f, GetScreenHeight() / 2.f};
-    PlayerB.position = (Vector2){GetScreenWidth() - 20.f, GetScreenHeight() / 2.f};
-    Ball.position = (Vector2){(GetScreenWidth() / 2.f), (GetScreenHeight() / 2.f)};
-
-    debugText = (char *)malloc(sizeof(char) * 100);
-    playerAScoreStr = (char *)malloc(sizeof(char) * 100);
-    playerBScoreStr = (char *)malloc(sizeof(char) * 100);
+bool player_can_move(PlayerRacket player_racket) {
+    return (player_racket.base.position.y >=
+            g_racket_texture.height / 2.0f / player_racket.frame_count) &&
+           ((GetScreenHeight() - g_racket_texture.height / 2.0f / player_racket.frame_count));
 }
 
-// draws texture on screen centered
-void DrawTextureOnScreen(Texture2D texture, Rectangle source, Vector2 position, float frameCount,
-                         Color color) {
-    DrawTextureRec(
-        texture, source,
-        (Vector2){position.x - texture.width / 2.f, position.y - texture.height / 2.f / frameCount},
-        color);
+void player_move(PlayerRacket player_racket, Vector2 move_dir) {
+    player_racket.base.position.y += g_player_b.speed * Vector2Length(move_dir);
 }
 
-void Update() {
-    if (IsKeyDown(KEY_W) && PlayerA.position.y >= racketTexture.height / 2.f / NUM_FRAMES) {
-        PlayerA.position.y -= racketSpeed;
-    } else if (IsKeyDown(KEY_S) &&
-               PlayerA.position.y <= GetScreenHeight() - racketTexture.height / 2.f / NUM_FRAMES) {
-        PlayerA.position.y += racketSpeed;
-    }
+/* TODO: Make the ball go to the serving player */
+void reset_positions() {
+    g_player_a.base.position = PLAYER_A_START_POS;
+    g_player_b.base.position = PLAYER_B_START_POS;
+    g_ball.base.position = BALL_START_POS;
+}
 
-    if (IsKeyDown(KEY_UP) && PlayerB.position.y >= racketTexture.height / 2.f / NUM_FRAMES) {
-        PlayerB.position.y -= racketSpeed;
-    } else if (IsKeyDown(KEY_DOWN) &&
-               PlayerB.position.y <= GetScreenHeight() - racketTexture.height / 2.f / NUM_FRAMES) {
-        PlayerB.position.y += racketSpeed;
-    }
+/* Set initial values */
+void init() {
+    g_racket_texture = LoadTexture("resources/racket.png");
+    g_ball_texture = LoadTexture("resources/ball.png");
+
+    g_player_a.base.texture = g_player_b.base.texture = g_racket_texture;
+    g_ball.base.texture = g_ball_texture;
+
+    Rectangle racket_rec = (Rectangle){0, 0, (float)g_racket_texture.width,
+                                       (float)g_racket_texture.height / g_player_a.frame_count};
+    Rectangle ball_rec =
+        (Rectangle){0, 0, (float)g_ball_texture.width, (float)g_ball_texture.height};
+
+    g_player_a.rect = g_player_b.rect = racket_rec;
+    g_ball.rect = ball_rec;
+
+    reset_positions();
+}
+
+void update() {
+    Vector2 player_a_move_dir = (Vector2){(int)IsKeyDown(KEY_W), (int)IsKeyDown(KEY_S)};
+    Vector2 player_b_move_dir = (Vector2){(int)IsKeyDown(KEY_UP), (int)IsKeyDown(KEY_DOWN)};
+
+    player_move(g_player_a, player_a_move_dir);
+    player_move(g_player_b, player_b_move_dir);
 
     if (IsKeyPressed(KEY_SPACE)) {
-        bBallLaunched = true;
+        g_game_state = ONGOING;
     }
 
     if (bBallLaunched) {
@@ -137,6 +150,15 @@ void Update() {
     sprintf(playerBScoreStr, "%i", PlayerBScore);
 }
 
+/* Draws texture on screen centered */
+void draw_texture_on_screen(Texture2D texture, Rectangle source, Vector2 position,
+                            float frame_count, Color color) {
+    DrawTextureRec(texture, source,
+                   (Vector2){position.x - texture.width / 2.f,
+                             position.y - texture.height / 2.f / frame_count},
+                   color);
+}
+
 int main() {
     // Initialization
     //-------------------------------------------------------------------------------------
@@ -154,7 +176,7 @@ int main() {
     while (!WindowShouldClose())  // Detect window close button or ESC key
     {
         // Update
-        Update();
+        update();
 
         // Draw
         //----------------------------------------------------------------------------------
@@ -162,9 +184,9 @@ int main() {
 
         ClearBackground(RAYWHITE);
 
-        DrawTextureOnScreen(racketTexture, racketRec, PlayerA.position, NUM_FRAMES, WHITE);
-        DrawTextureOnScreen(racketTexture, racketRec, PlayerB.position, NUM_FRAMES, WHITE);
-        DrawTextureOnScreen(ballTexture, ballRec, Ball.position, 1, WHITE);
+        draw_texture_on_screen(racketTexture, racketRec, PlayerA.position, NUM_FRAMES, WHITE);
+        draw_texture_on_screen(racketTexture, racketRec, PlayerB.position, NUM_FRAMES, WHITE);
+        draw_texture_on_screen(ballTexture, ballRec, Ball.position, 1, WHITE);
 
         DrawFPS(10, 420);
 
